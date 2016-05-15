67a30fcb6e106b6804ea2610892e1f5c0dba69c8
dannichols <danielnichols@Daniel-Nicholss-MacBook-Pro.local>
2016-05-15 14:32:42 -0400

    Added TV and watch targets

---
+++ HDOPromise/HDOPromiseTV/HDOPromiseTV.h
@@ -0,0 +1,19 @@
+//
+//  HDOPromiseTV.h
+//  HDOPromiseTV
+//
+//  Created by Daniel Nichols on 5/15/16.
+//  Copyright © 2016 Hey Danno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDOPromiseTV.
+FOUNDATION_EXPORT double HDOPromiseTVVersionNumber;
+
+//! Project version string for HDOPromiseTV.
+FOUNDATION_EXPORT const unsigned char HDOPromiseTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDOPromiseTV/PublicHeader.h>
+
+
---
+++ HDOPromise/HDOPromiseWatch/HDOPromiseWatch.h
@@ -0,0 +1,19 @@
+//
+//  HDOPromiseWatch.h
+//  HDOPromiseWatch
+//
+//  Created by Daniel Nichols on 5/15/16.
+//  Copyright © 2016 Hey Danno. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for HDOPromiseWatch.
+FOUNDATION_EXPORT double HDOPromiseWatchVersionNumber;
+
+//! Project version string for HDOPromiseWatch.
+FOUNDATION_EXPORT const unsigned char HDOPromiseWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDOPromiseWatch/PublicHeader.h>
+
+
