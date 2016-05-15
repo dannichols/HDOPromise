9ab7fd1b641f9f06dc4541f82f25ad5acd3befc2
dannichols <danielnichols@Daniel-Nicholss-MacBook-Pro.local>
2016-05-15 15:16:01 -0400

    Added podspec

--- HDOPromise/HDOPromise/HDOPromise.h
+++ HDOPromise/HDOPromise/HDOPromise.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Hey Danno. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for HDOPromise.
 FOUNDATION_EXPORT double HDOPromiseVersionNumber;
